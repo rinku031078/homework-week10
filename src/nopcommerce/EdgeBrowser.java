908cde6714eca94101406fcda1cbedb9fe41fc40
rinku031078 <anandtri78@gmail.com>
2021-11-08 17:59:50 +0000

    This is my week 10 homework

---
+++ src/nopcommerce/EdgeBrowser.java
@@ -0,0 +1,19 @@
+package nopcommerce;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class EdgeBrowser {
+
+    public static void main(String[] args) {
+        String baseUrl = "https://demo.nopcommerce.com/";
+        //String baseUrl = "https://www.google.com/";
+        System.setProperty("webdriver.edge.driver", "drivers/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
+        driver.get(baseUrl);
+        driver.manage().window().maximize();//maximise
+        String getTitle = driver.getTitle();
+        System.out.println(getTitle);
+        driver.close();
+    }
+}
---
+++ src/nopcommerce/FirefoxBrowser.java
@@ -0,0 +1,18 @@
+package nopcommerce;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirefoxBrowser {
+
+    public static void main(String[] args) {
+        String baseUrl = "https://demo.nopcommerce.com/";
+        System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get(baseUrl);
+        driver.manage().window().maximize();//maximise
+        String getTitle = driver.getTitle();
+        System.out.println(getTitle);
+        driver.close();
+    }
+}
